2fee4d57c8d6be2f318710c033652d26c2a95f18
torehc <torehc@gmail.com>
2018-11-08 20:27:04 +0000

    Añadido codígo de la clase para acceder a las variables de la clase

--- lib/tdd_lpp/tdd_lpp.rb
+++ lib/tdd_lpp/tdd_lpp.rb
@@ -1,12 +1,42 @@
 module TddLpp
   class InfoNutricional
 
-    def initialize(nombre)
+    def initialize(nombre, grasas, grasas_saturadas, hidratos_carbono, azucares, proteinas, sal)
       @nombre = nombre
+      @grasas = grasas
+      @grasas_saturadas = grasas_saturadas
+      @hidratos_carbono = hidratos_carbono
+      @azucares = azucares
+      @proteinas = proteinas
+      @sal = sal
     end
 
     def nombre
       @nombre
+    end
+
+    def grasas
+      @grasas
+    end
+
+    def grasas_saturadas
+      @grasas_saturadas
+    end
+
+    def hidratos_carbono
+      @hidratos_carbono
+    end
+
+    def azucares
+      @azucares
+    end
+
+    def proteinas
+      @proteinas
+    end
+
+    def sal
+      @sal
     end
 
   end
