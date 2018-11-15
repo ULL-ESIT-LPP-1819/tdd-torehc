629b5dd18620edc5c1195e856d53c03c410e59f4
torehc <torehc@gmail.com>
2018-11-15 17:53:56 +0000

    Añadido código para insertar en lista

--- lib/tdd_lpp/lista.rb
+++ lib/tdd_lpp/lista.rb
@@ -7,10 +7,17 @@ class Lista
   attr_accessor :array, :head, :tail
 
   def initialize(vector)
-
     @array = vector
     @head = @array[0]
     @tail = @array[-1]
   end
-
+"""
+  def push_back(objeto_nodo)
+    @array << objeto_nodo
+    @array[0].prev = @array[-1].value
+    @array[-2].next = @array[-1].value
+    @tail = objeto_nodo
+    return tail.value
+  end
+"""
 end
