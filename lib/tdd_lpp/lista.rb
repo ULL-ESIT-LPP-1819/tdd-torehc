c2dd8de069a81b8dd8ea7e495186a15a912e7324
torehc <torehc@gmail.com>
2018-11-15 17:43:55 +0000

    Código inicial de la lista

--- lib/tdd_lpp/lista.rb
+++ lib/tdd_lpp/lista.rb
@@ -1,2 +1,16 @@
 # create a Struct with :value, :next and :prev
 Nodo = Struct.new(:value, :next, :prev)
+
+class Lista
+
+  include Enumerable
+  attr_accessor :array, :head, :tail
+
+  def initialize(vector)
+
+    @array = vector
+    @head = @array[0]
+    @tail = @array[-1]
+  end
+
+end
