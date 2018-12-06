1b739974ee9e5910f9183fab99080f7f0970bfd9
torehc <torehc@gmail.com>
2018-12-06 23:05:25 +0000

    Añadidos comentarios a la clase lista para explicar su funcionamiento

--- lib/tdd_lpp/lista.rb
+++ lib/tdd_lpp/lista.rb
@@ -1,17 +1,24 @@
 # create a Struct with :value, :next and :prev
 Nodo = Struct.new(:value, :next, :prev)
 
+# Clase Lista
+#
 class Lista
 
   include Enumerable
+  # Atributos con acceso
   attr_accessor :array, :head, :tail
 
+  # Inicialización de la clase
+  # @param [vector] Array de objetos para la lista
   def initialize(vector)
     @array = vector
     @head = @array[0]
     @tail = @array[-1]
   end
 
+  # Añadir elemento al final de la lista
+  # @param [objeto_nodo] Objeto para introducir
   def push_back(objeto_nodo)
     @array << objeto_nodo
     @array[0].prev = @array[-1].value
@@ -20,6 +27,7 @@ class Lista
     return tail.value
   end
 
+  # Sacar elemento del principio
   def pop_front
     @array[-1].next = @array[1].value
     @array[1].prev = @array[-1].value
@@ -29,6 +37,7 @@ class Lista
     return @head.value
   end
 
+  # Sacar elemento
   def pop
     @array[-2].next = @array[0].value
     @array[0].prev = @array[-2].value
@@ -38,14 +47,19 @@ class Lista
     return @tail.value
   end
 
+  # Comprobar si la lista está vacía
+  # @return [bool] lista vacía
   def empty
     return @array.empty?
   end
 
+  # Ordena con sort los pacientes por su IMC
+  # @return [IMC] Devuelve el IMC mínimo
   def sort_paciente_IMC
     return @array.sort_by { |h| h.value.estadoNutricional.IMC() }
   end
 
+  # Método each para Enumerable
   def each(&block)
     @array.each(&block)
   end
